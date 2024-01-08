01f95c0057841e9ceaf694c1c4728844fcaaddf9
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-01-08 12:05:30 +0330

    fix some bugs

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,25 +2,13 @@
 import React from 'react';
 import connectDB from './api/db';
 import Post from '../models/Post';
-<<<<<<< HEAD
-import Layout from '../components/Dashboard_Layout.js';
-=======
 import Layout from '../components/inLayout';
 import styles from '../styles/showpost.module.css';
->>>>>>> upload-using-s3
 
 const Home = ({ posts }) => {
   return (
     <Layout>
       <div>
-<<<<<<< HEAD
-        <h1>My Blog</h1>
-        <ul>
-          {posts.map((post) => (
-            <li key={post._id}>
-              <h2>{post.title}</h2>
-              <p>{post.content}</p>
-=======
         <h1>Liara Blog</h1>
         <ul>
           {posts.map((post) => (
@@ -28,7 +16,6 @@ const Home = ({ posts }) => {
               <h2 className={styles.postTitle}>{post.title}</h2>
               <p className={styles.postContent}>{post.content}</p>
               {post.image && <img src={`/uploads/${post.image}`} alt={post.title} className={styles.postImage} />}
->>>>>>> upload-using-s3
             </li>
           ))}
         </ul>
